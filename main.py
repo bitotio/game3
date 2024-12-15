e64505732f0c347504e5a08bae56e57108b08e94
welesi <gsf58381@gmail.com>
2024-12-15 14:51:12 +0300

    made main
    Доработал файл main. Игра работает

--- main.py
+++ main.py
@@ -1,13 +1,54 @@
 from tkinter import *
 
 def set_status(text, color='black'):
-    pass
+    canvas.itemconfig(text_id, text=text, fill=color)
 
 def key_handler(event):
-    pass
+    # if event.keycode == KEY_UP:
+    #     menu_up()
+    # if event.keycode == KEY_DOWN:
+    #     menu_down()
+    # if event.keycode == KEY_ENTER:
+    #     menu_enter()
+
+    if game_over:
+        return
+    # if event.keycode == KEY_PAUSE:
+    #     pause_toggle()
+    #
+    # if pause:
+    #     return
+    # if event.keycode == KEY_ESC:
+    #     menu_toggle()
+    #
+    # if menu_mode:
+    #     return
+
+    set_status('Вперед!')
+    if event.keycode == KEY_PLAYER1:
+        canvas.move(player1, SPEED, 0)
+    if event.keycode == KEY_PLAYER2:
+        canvas.move(player2, SPEED, 0)
+
+    check_finish()
 
 def check_finish():
-    pass
+    global game_over
+    coords_player1 = canvas.coords(player1)
+    coords_player2 = canvas.coords(player2)
+    coords_finish = canvas.coords(finish_id)
+
+    x1_right = coords_player1[2]
+    x2_right = coords_player2[2]
+    x_finish = coords_finish[0]
+
+    if x1_right >= x_finish:
+        set_status('Победа верхнего игрока', player1_color)
+        game_over = True
+
+    if x2_right >= x_finish:
+        set_status('Победа нижнего игрока', player2_color)
+        game_over = True
 
 game_width = 800
 game_height = 800
@@ -38,7 +79,7 @@ window.title('Меню игры')
 
 canvas = Canvas(window, width=game_width, height=game_height, bg='white')
 canvas.pack()
-menu_create(canvas)
+# menu_create(canvas)
 player1 = canvas.create_rectangle(x1,
                                   y1,
                                   x1 + player_size,
@@ -62,5 +103,5 @@ text_id = canvas.create_text(x1,
                              text='Вперед!')
 
 
-#window.bind('<KeyRelease>', key_handler)
+window.bind('<KeyRelease>', key_handler)
 window.mainloop()
